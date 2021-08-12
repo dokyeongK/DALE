b1ccc9c4b9b11a8de359d7aff7c520d33675df9d
dokyeongK <dkdk6638@gmail.com>
2021-08-12 10:29:10 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from option import args
 
 if __name__ == "__main__":
     opt = args
-    running_type = opt.running_type
+    # To run the train_xx.py files!
+    # running_type = opt.running_type
     # Select running type
 
